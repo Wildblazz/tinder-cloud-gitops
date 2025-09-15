1a969d07fefbfe92e3e741abe647dec3a43e5cdf
okovalenko <wildblazz@gmail.com>
2025-09-15 09:44:46 +0200

    ArgoCd: Added Ingress-kong
    ArgoCd: Added Ingress-kong
    
    ArgoCd: Added Ingress-kong
    
    ArgoCd: Added Ingress-kong

--- argocd-init.sh
+++ argocd-init.sh
@@ -4,4 +4,6 @@ kubectl create namespace argocd
 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+kubectl port-forward svc/argocd-server -n argocd 8444:443 &
+
+sudo kubectl -n ingress-kong port-forward service/kong-proxy 80:80 &
